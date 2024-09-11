5515fe8104cbd4b1e349de441e5deabda8560b9f
blessingebele <blessmart.com@gmail.com>
2024-09-12 00:14:38 +0100

    hours to seconds

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -4,11 +4,10 @@
  
 
 hours = 2
-seconds_per_minutes = 60
-minutes_in_two_hours = 120
+seconds_per_hour = 3600
 
 #converting 2 hours to seconds
 #since 60 seconds make 1 minutes, 60 minutes is equal to 1 hours. therefore seconds in 2 hours eguals:
  
-seconds = minutes_in_two_hours * seconds_per_minutes
+seconds = hours * 3600
 print("2 hour(s) is", seconds, 'seconds')
