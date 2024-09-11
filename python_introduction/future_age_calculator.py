cc5eef1948d07c3863b60cad8a11912404976d60
blessingebele <blessmart.com@gmail.com>
2024-09-12 00:29:46 +0100

    future age calculator

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,10 @@
+#author: Blessing Ebele Anochili 
+#date: 11/09/2024
+#purpose: A python script that asks the user for their current age and then calculates how old they will be in a specific future year. 
+
+#getting an input from the user
+current_age = int(input("How old are you? "))
+
+#calculating the users age in 2050
+age = current_age + 27
+print("In 2050, you will be", age, 'years old.')
