f1fca190a9d7c94ce9c9ddf163d0361a0cd2b459
blessingebele <blessmart.com@gmail.com>
2024-09-11 23:21:08 +0100

    simple interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,10 +1,10 @@
 #author: Blessing Ebele Anochili 
 #date: 11/09/2024
-#purpose: this scrpt calculates the simple intrest of the data provided below.
+#purpose: this script calculates the simple intrest of the data provided below.
 
 principal = 1000
-rate_per_annum = 0.05
-time_in_year = 3
+rate = 0.05
+time = 3
 
-interest = principal * rate_per_annum * time_in_year
+interest = principal * rate * time
 print("The simple interest is", float(interest))
