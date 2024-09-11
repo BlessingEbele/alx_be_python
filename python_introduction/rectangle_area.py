eb3fa3db2cf37979ef9a8946f1befc9196b74d7f
blessingebele <blessmart.com@gmail.com>
2024-09-11 23:28:24 +0100

    rectangle_area.py

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,13 @@
+#author: Blessing Ebele Anochili 
+#date: 11/09/2024
+#purpose: this python script calculates the area of a rectangle 
+
+length = 10
+width = 5
+area = 0
+ 
+#calculating the area 
+area = length * width 
+
+#printing the result
+print("The area of the rectangle is", area)
