b8e3d46a6b0fa683725d64b22e0ddff88c9a13b6
blessingebele <blessmart.com@gmail.com>
2024-09-18 01:05:15 +0100

    choice operation

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,12 @@
+#author: Blessing Ebele Anochili
+#date: 18/09/2024
+#purpose: This calculator will prompt the user to enter two numbers and select an operation (addition, subtraction, multiplication, or division). The script will then perform the selected operation using a Match Case statement and display the result.
+
+#collecting input from user
+num1 = float(input("Enter the first number:"))
+num2 = float(input("Enter the second number:"))
+
+choice_operation = input("Choose the operation (+, -, *, /):.")
+result = 
+ match choice_operation
+case _ if choice_operation == + 
