229279e49de3e138028d43d2a3af8359109e9941
blessingebele <blessmart.com@gmail.com>
2024-09-23 23:11:48 +0100

    arithmetic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -3,6 +3,7 @@
 # purpuse : In this script, define a function that performs basic arithmetic operations. This function, perform_operation, will be imported and used in a separate main.py script, which we provide.
 # arithmetic_operations.py
 
+
 def perform_operation(num1, num2, operation):
     # Perform the arithmetic operation based on the input
     if operation == 'add':
@@ -12,9 +13,9 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         return num1 * num2
     elif operation == 'divide':
-        if num2 != 0:
-            return num1 / num2
-        else:
+        if num2 == 0:
             return "Error: Cannot divide by zero"
+        else:
+            return num1 / num2
     else:
         return "Error: Invalid operation"
