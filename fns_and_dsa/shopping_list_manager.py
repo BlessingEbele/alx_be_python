37ab41ab9d5061ae7a2c36af646a3edfc1880609
blessingebele <blessmart.com@gmail.com>
2024-10-24 02:36:45 +0100

    catch up week

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -8,7 +8,7 @@
 
 # Global Conversion Factors
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+CELSIUS_TO_FAHRENHEIT_FACTORS = 9 / 5
 FREEZING_POINT_DIFFERENCE = 32
 
 # Function to convert Fahrenheit to Celsius
